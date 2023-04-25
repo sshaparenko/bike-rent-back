8431676ffcf40375af0be266d1160161f60da262
sshaparenko <zshaparenkoz@gmail.com>
2023-04-25 20:35:42 +0300

    update tempates dir path

--- app/core/settings.py
+++ app/core/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'app/templates'],
+        'DIRS': [BASE_DIR / 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
