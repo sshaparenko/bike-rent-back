e0a61fc499a7e2b6961cf40d29022f2d1ffa2ec8
sshaparenko <zshaparenkoz@gmail.com>
2023-05-19 17:52:33 +0300

    registering models for admin panel

--- app/apps/bike_rent/admin.py
+++ app/apps/bike_rent/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Bike)
+admin.site.register(models.BikeCategory)
+admin.site.register(models.RentOrder)
